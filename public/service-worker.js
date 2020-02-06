f4bf20d77b55c2095bb2a401ac10c5a6f3fa2484
Aidan Willett <Gamer9310@gmail.com>
2020-02-06 09:47:34 -0800

    try 2

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,17 +4,16 @@ const FILES_TO_CACHE = [
   "/db.js",
   "/index.js",
   "/style.css",
-  "/favicon.ico",
   "/service-worker.js",
   "manifest.webmanifest",
-  "assets/images/icons/icon-72x72.png",
-  "assets/images/icons/icon-96x96.png",
-  "assets/images/icons/icon-128x128.png",
-  "assets/images/icons/icon-144x144.png",
-  "assets/images/icons/icon-152x152.png",
-  "assets/images/icons/icon-192x192.png",
-  "assets/images/icons/icon-384x384.png",
-  "assets/images/icons/icon-512x512.png"
+  "images/icons/icon-72x72.png",
+  "images/icons/icon-96x96.png",
+  "images/icons/icon-128x128.png",
+  "images/icons/icon-144x144.png",
+  "images/icons/icon-152x152.png",
+  "images/icons/icon-192x192.png",
+  "images/icons/icon-384x384.png",
+  "images/icons/icon-512x512.png"
 ]
 
 const CACHE_NAME = "static-cache-v2";
